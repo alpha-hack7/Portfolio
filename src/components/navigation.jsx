c2d46f86f5f2eb3a548feebf08cb5ab1aa730c7c
Alpha <alphaindeje@gmail.com>
2025-10-09 13:51:08 +0300

    navbar -1

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -1,11 +1,51 @@
+import React, { useState, useEffect, useRef } from "react";
+import "../css/portfolio.css";
+
 const Navigation = () => {
+	const [activeSection, setActiveSection] = useState("home");
+	const sections = ["home", "about", "projects", "contact"];
+
+	useEffect(() => {
+		const options = {
+			threshold: 0.6, // Trigger when 60% of the section is visible
+		};
+		const observer = new IntersectionObserver((entries) => {
+			entries.forEach((entry) => {
+				if (entry.isIntersecting) {
+					setActiveSection(entry.target.id);
+				}
+			});
+		}, options);
+
+		sections.forEach((section) => {
+			const element = document.getElementById(section);
+			if (element) {
+				observer.observe(element);
+			}
+		});
+
+		return () => {
+			observer.disconnect();
+		};
+	}, []);
 	return (
 		<>
 			<nav>
-				<a href="#home">Home</a>
-				<a href="#about">About Me</a>
-				<a href="#projects">My Projects</a>
-				<a href="#contact">Contact Me</a>
+				<a href="#home" className={activeSection === "home" && "active"}>
+					Home
+				</a>
+				<a href="#about" className={activeSection === "about" && "active"}>
+					About Me
+				</a>
+				<a
+					href="#projects"
+					className={activeSection === "projects" && "active"}
+				>
+					My Projects
+				</a>
+				<a href="#contact" className={activeSection === "contact" && "active"}>
+					Contact Me
+				</a>
 			</nav>
 			<div
 				className="hamburger"
